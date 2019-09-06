778f71df27f2440904fcb75be5de454fb60b75f2
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-09-06 08:29:31 +0800

    working on automation

--- check_missing.sh
+++ check_missing.sh
@@ -2,7 +2,8 @@ NUMBER_TILES=4
 DATA=data/
 NUMBER_F=`ls $DATA/*/*.txt -1 | wc -l`
 
-echo "Total files in" $DATA "directory is" $NUMBER_F
+
+#echo "Total files in" $DATA "directory is" $NUMBER_F
 
 for d in $DATA/*/ ; 
 do
@@ -12,9 +13,16 @@ do
         for i in "rf0XX" "rf0YY" "rf1XX" "rf1YY"; do 
             
             if [ ! -f $d/$i* ]; then
-                echo "$i"_"$TIME".txt" file not found"
+                echo "$i"_"$TIME".txt" file not found" >> status.txt
             fi    
         
         done
     fi    
 done
+
+if [ $NUMBER_F != $(($NUMBER_TILES*48)) ]; then
+    echo "Heeeelllllp! There are files missing in the data directory!" >> status.txt
+    echo "I have" $NUMBER_F "files instead of" $(($NUMBER_TILES*48))"." >> status.txt
+else
+    echo "There are no missing files in the data directory. All "$NUMBER_F" files are ready to be transferred to Ozstar" >> status.txt 
+fi    
--- one_script_to_rule_them_all.sh
+++ one_script_to_rule_them_all.sh
@@ -2,14 +2,17 @@
 # Will create an at queue of 30 minute jobs from 6AM - 6AM 
 
 echo "touch status.txt" | at 05:40
-echo "printf \"Good morning Aman, Jack, Nichole!\n \nIt's 5:40AM and I have woken up to schedule a new day of jobs. \n \" >> status.txt" | at 05:40
+echo "printf \"Good morning Aman, Jack, Nichole!\n \nIt is 5:40AM and I've woken up to schedule a new day of jobs. \n \" >> status.txt" | at 05:40
 at 05:45 < make_night_schedule.sh 
 
 # At 6:10AM, check to see if there are 24*2*4 files in ./data/*/*
 # if there are, source do_rsync.sh then source clear_data.sh
 
+echo "echo \"It is 6:10AM. Starting to check for missing data files from the last 24 hours.\" >> status.txt" | at 06:10
+at 06:10 < check_missing.sh
 
 # Else, source do_rsync.sh 
 # At 6:15AM send emails. If files are missing, send mails to Jack & Nichole, otherwise, only to me
 
+at 6:15 < email.sh
 echo "rm status.txt" | at 06:20
