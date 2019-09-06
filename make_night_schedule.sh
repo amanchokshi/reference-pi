bbef4d9d5fad4c4244d4269d6278c32ff5e41251
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-09-06 15:13:15 +0800

    confused now

--- make_night_schedule.sh
+++ make_night_schedule.sh
@@ -1,13 +1,13 @@
 # This script creates a queue of at jobs for a 24 hour period
 # Needs to be executed before 6:00AM everyday
 
-OBS_LENGHT=1800
-TODAY=`date +'%Y-%m-%d-%H:%M' -d '6am'`
-TOMORROW=`date +'%Y-%m-%d-%H:%M' -d '6am +1 day'`
+#OBS_LENGHT=1800
+#TODAY=`date +'%Y-%m-%d-%H:%M' -d '6am'`
+#TOMORROW=`date +'%Y-%m-%d-%H:%M' -d '6am +1 day'`
 
-# START=2019-09-06-09:30
-# STOP=2019-09-06-21:30
-# OBS_LENGHT=1800
+START=2019-09-06-14:37
+STOP=2019-09-06-14:40
+OBS_LENGHT=60
 
 
 python make_night_schedule.py --time_obs=$OBS_LENGHT --start_date=$START \
